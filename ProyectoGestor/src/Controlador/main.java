112fd72ffb7bea2919541d2de001d2b4cf9c5fb1
DjXaTo <djxato@gmail.com>
2016-11-14 20:00:06 +0100

    Version 20161114 2000	primer push

--- ProyectoGestor/src/Controlador/main.java
+++ ProyectoGestor/src/Controlador/main.java
@@ -1,14 +1,9 @@
 package Controlador;
 
-/**
- *
- * @author AlbertoManuel
- */
+
 public class main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
